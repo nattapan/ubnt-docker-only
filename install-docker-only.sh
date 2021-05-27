85608989c702f91203e85e0fac8a55a71c5b47aa
nattapan <3727102+nattapan@users.noreply.github.com>
2021-05-27 15:26:10 +0700

    Update install-docker-only.sh

--- install-docker-only.sh
+++ install-docker-only.sh
@@ -19,5 +19,5 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
    sudo apt-get install -y docker-ce docker-ce-cli containerd.io
    
    sudo curl -L "https://github.com/docker/compose/releases/download/1.27.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-   sudo chmod +x /usr/local/bin/docker-compose
+   sudo chmod +rx /usr/local/bin/docker-compose
    
